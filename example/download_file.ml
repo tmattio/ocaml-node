8085aef4ac73e8846d4c76eb3ab5343360c182bc
Thibaut Mattio <thibaut.mattio@gmail.com>
2021-03-16 12:44:14 +0100

    Update download_file example

--- example/download_file.ml
+++ example/download_file.ml
@@ -1,3 +1,15 @@
 open Node
 
-let () = Console.info "Hello World!"
+let () =
+  let file = Fs.createWriteStream "file.jpg" () in
+  let (_ : Http.ClientRequest.t) =
+    Https.get
+      "http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg"
+      ~callback:(fun response ->
+        let (_ : Stream.Writable.t) =
+          Http.IncomingMessage.pipe response file ()
+        in
+        ())
+      ()
+  in
+  ()
